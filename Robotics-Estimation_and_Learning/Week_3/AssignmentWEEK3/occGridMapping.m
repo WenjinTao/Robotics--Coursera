2eec49145ddfff5e4c8d754460ad722af9ee6026
tavjo <wenjin_tao@126.com>
2017-01-19 17:02:14 -0600

    add week_3 of Estimation and Learning

---
+++ Robotics-Estimation_and_Learning/Week_3/AssignmentWEEK3/occGridMapping.m
@@ -0,0 +1,46 @@
+% Robotics: Estimation and Learning 
+% WEEK 3
+% 
+% Complete this function following the instruction. 
+function myMap = occGridMapping(ranges, scanAngles, pose, param)
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%5
+% Parameters 
+% 
+% % the number of grids for 1 meter.
+% myResol = param.resol;
+% % the initial map size in pixels
+% myMap = zeros(param.size);
+% % the origin of the map in pixels
+% myorigin = param.origin; 
+% 
+% % 4. Log-odd parameters 
+% lo_occ = param.lo_occ;
+% lo_free = param.lo_free; 
+% lo_max = param.lo_max;
+% lo_min = param.lo_min;
+
+% N = size(pose,2);
+% for j = 1:N % for each time,
+% 
+%       
+%     % Find grids hit by the rays (in the gird map coordinate)
+%   
+% 
+%     % Find occupied-measurement cells and free-measurement cells
+%    
+% 
+%     % Update the log-odds
+%   
+% 
+%     % Saturate the log-odd values
+%     
+% 
+%     % Visualize the map as needed
+%    
+% 
+% end
+
+end
+
